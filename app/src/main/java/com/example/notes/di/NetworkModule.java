1aff2effa4306090f6c0df804547fbf2d44961c7
Andrey Lebedev <lebedevav2@sibintek.ru>
2021-01-23 15:19:56 +0300

    Minor style fixes

--- app/src/main/java/com/example/notes/di/NetworkModule.java
+++ app/src/main/java/com/example/notes/di/NetworkModule.java
@@ -1,7 +1,12 @@
 package com.example.notes.di;
 
+import android.content.Context;
+
 import com.example.notes.model.NetworkUtils;
 import com.example.notes.model.ServerApi;
+import com.franmontiel.persistentcookiejar.PersistentCookieJar;
+import com.franmontiel.persistentcookiejar.cache.SetCookieCache;
+import com.franmontiel.persistentcookiejar.persistence.SharedPrefsCookiePersistor;
 import com.google.gson.GsonBuilder;
 
 import javax.inject.Singleton;
@@ -9,6 +14,8 @@ import javax.inject.Singleton;
 import dagger.Module;
 import dagger.Provides;
 import io.reactivex.schedulers.Schedulers;
+import okhttp3.CookieJar;
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -18,11 +25,24 @@ public class NetworkModule {
 
     @Singleton
     @Provides
-    NetworkUtils provideNetworkUtils() {
+    CookieJar provideCookieJar(Context ctx) {
+        return new PersistentCookieJar(new SetCookieCache(), new SharedPrefsCookiePersistor(ctx));
+    }
+
+    @Singleton
+    @Provides
+    OkHttpClient provideOkHttpCLient(CookieJar cookieJar) {
+        return new OkHttpClient.Builder().cookieJar(cookieJar).build();
+    }
+
+    @Singleton
+    @Provides
+    NetworkUtils provideNetworkUtils(OkHttpClient okHttpClient) {
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://45.88.79.176:8080/api/")
+                .baseUrl("https://gastar.ga/api/")
                 .addConverterFactory(GsonConverterFactory.create(new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").create()))
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+                .client(okHttpClient)
                 .build();
 
         ServerApi serverApi = retrofit.create(ServerApi.class);
